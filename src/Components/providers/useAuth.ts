3220fa14f200509d828d44e61641f95521b98998
Konstantin <KonstantKar@yandex.ru>
2023-07-11 14:55:02 +0300

    Удаление лишних импортов и добавление компоненты Div

--- src/Components/providers/useAuth.ts
+++ src/Components/providers/useAuth.ts
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
 
 const useAuth = () => {
-  // Данный хук нужен чтобы не экспортировать каждый раз Context
+  // Данный хук нужен чтобы не экспортировать каждый раз useContext и доставать нужный элемент через useAuth()
   const value = useContext(AuthContext);
   return value;
 };
